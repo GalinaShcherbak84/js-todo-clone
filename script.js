4b33a6f1cd7daf21c260c0782f1c45ec2dca8dc8
GalinaSherbak84 <galinasherbak@gmail.com>
2021-04-14 23:11:56 +0200

    Rendi todo completato / da fare

--- script.js
+++ script.js
@@ -28,13 +28,59 @@ $(document).ready(function(){
     var template = $('#template li');
 
     // 1 popolazione todo list
+
     for(var i = 0; i < todoItems.length; i++){
         var todo = todoItems[i];
+
         // template
         var item = template.clone();
         item.find('.text').text(todo.text);
+
         // aggiunta alla lista
         list.append(item);
     }
+    // 2 inserimento nuovo todo
+
+    newInput.keyup(function(event){
+        console.log(event.which);
+
+        if(event.which === 13){
+
+            //console.log('enter');
+
+            var text = newInput.val().trim();
+            if(text !== ''){
+                console.log(text);
+                
+                // template
+                var item  = template.clone();
+                item.find('.text').text(text);
+
+                // aggiunta alla lista
+                list.append(item);
+
+                // reset
+                newInput.val('');
+            }
+        }
+    });
+
+    // 3 rimozione todo item
+
+    // a
+    /* list.find('li i').click(function(){
+        $(this).parent().remove();
+    }); */
+
+    // b
+    $('body').on('click', '.todos li i', function(){
+        $(this).parent().remove();
+    });
+
+    // 4 Rendi todo completato / da fare
+    $('body').on('click', '.todos li span', function(){
+        $(this).toggleClass('completed');
+    })
+    
 
 });
