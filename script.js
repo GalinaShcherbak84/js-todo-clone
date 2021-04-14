2057d8726e93e04bc2d0f1fb01cb14ca0dafa1f3
GalinaSherbak84 <galinasherbak@gmail.com>
2021-04-14 22:06:18 +0200

    aggiungere alla lista il template

--- script.js
+++ script.js
@@ -1,3 +1,40 @@
 $(document).ready(function(){
+
+    // sorgente dati esterna
+    var todoItems = [
+        {
+            text: 'Prendere il latte',
+            completed: false,
+        },
+        {
+            text: 'Fare benzina',
+            completed: true,
+        },
+        {
+            text: 'Chiamare il cliente',
+            completed: false,
+        },
+        {
+            text: 'Trovare l\'articolo',
+            completed: true,
+        },
+    ];
+        
     
+
+    // references
+    var list = $('.todos');
+    var newInput = $('.add-todo');
+    var template = $('#template li');
+
+    // 1 popolazione todo list
+    for(var i = 0; i < todoItems.length; i++){
+        var todo = todoItems[i];
+        // template
+        var item = template.clone();
+        item.find('.text').text(todo.text);
+        // aggiunta alla lista
+        list.append(item);
+    }
+
 });
